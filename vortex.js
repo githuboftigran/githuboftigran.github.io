eac71c165e2055f22cee0612d645b4941629db9c
Tigran <mail.of.tigran@gmail.com>
2020-12-31 12:38:52 +0400

    * Colors for case added

--- vortex.js
+++ vortex.js
@@ -144,6 +144,7 @@ window.onload = function() {
 
     copyButton.addEventListener('click', () => {
         canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({'image/png': blob})]));
+        console.log('============== CPU =============');
         const allColors = [];
         for (let i = 0; i < 9; i+= 1) {
             const color = interpolateColor([0x9000ff, 0x0000ff, 0x00ffff], i / 8 );
@@ -161,7 +162,21 @@ window.onload = function() {
             allColors[(11 + i) % 15] = `${r}\t${g}\t${b}`;
         }
         allColors.forEach((color, index) => console.log(`${index}\t${color}`));
-        console.log('===========================');
+
+        allColors.length = 0;
+        console.log('============== Case =============');
+        for (let i = 0; i < 6; i+= 1) {
+            const color = interpolateColor([0x6000ff, 0x0000ff, 0x00ffff], i / 5 );
+            const r = (color >> 16) & 0xff;
+            const g = (color >> 8) & 0xff;
+            const b = color & 0xff;
+            const pos = (i + 8) % 12;
+            allColors[pos] = `${r}\t${g}\t${b}`;
+            allColors[(15 - pos) % 12] = `${r}\t${g}\t${b}`;
+        }
+        allColors.forEach((color, index) => console.log(`${index}\t${color}`));
+
+        console.log('============== RAM =============');
         for (let i = 0; i < 10; i+= 1) {
             const color = interpolateColor([0x6000ff, 0x0000ff, 0x00ffff], i / 9 );
             const r = (color >> 16) & 0xff;
